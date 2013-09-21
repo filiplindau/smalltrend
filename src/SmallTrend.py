55d8f634991dd699a17b8e0813cd1d0f919ae0fc
filiplindau <filip.lindau@maxlab.lu.se>
2013-09-21 02:12:16 +0200

    Clip rectangle

--- src/SmallTrend.py
+++ src/SmallTrend.py
@@ -5,8 +5,6 @@ Created on Aug 26, 2013
 @author: Filip
 """
 import numpy as np
-import PyTango as pt
-import pyqtgraph as pq
 from PyQt4 import QtGui, QtCore
 import sys
 import time
@@ -20,9 +18,9 @@ class SmallTrend(QtGui.QWidget):
 		QtGui.QWidget.__init__(self,parent)
 		
 		self.maxDataSize = 20000
-		self.xSpan = float(1800.0)
-		self.ySpan = float(800.0)
- 		self.axisOffset = 1
+		self.xSpan = float(60.0)
+		self.ySpan = float(1.0)
+ 		self.axisOffset = 3
 		self.xScale = (self.size().width() - self.axisOffset*2) / self.xSpan		
 		self.yScale = (self.size().height() - self.axisOffset*2) / self.ySpan
 
@@ -43,14 +41,20 @@ class SmallTrend(QtGui.QWidget):
 		self.curvePen.setWidthF(1.3)
 		self.curvePen.setCosmetic(True)		
 		crvFillColor = self.crvColor
-		crvFillColor.setAlphaF(0.2)
+		crvFillColor.setAlphaF(0.4)
 		self.curveBrush = QtGui.QBrush(crvFillColor)
 		self.curveBrush.setStyle = QtCore.Qt.SolidPattern
 		self.axisPen = QtGui.QPen(self.axisColor)
 		self.axisPen.setWidthF(1.0)
 		self.axisBrush = QtGui.QBrush()
 		self.axisBrush.setStyle = QtCore.Qt.NoBrush
-		self.axisFont = QtGui.QFont('arial', 8)
+		axisFillColor = QtGui.QColor(QtCore.Qt.black)
+		axisFillColor.setAlphaF(0.25)
+		self.axisBkgPen = QtGui.QPen(axisFillColor)
+		self.axisBkgBrush = QtGui.QBrush(axisFillColor)
+		self.axisBkgBrush.setStyle(QtCore.Qt.SolidPattern)
+		self.axisFont = QtGui.QFont('calibri', 8)
+		self.axisFont.setWeight(200)
 		self.axisFontHeight = QtGui.QFontMetricsF(self.axisFont).height()
 		
 		self.setMinimumWidth(50)
@@ -86,9 +90,9 @@ class SmallTrend(QtGui.QWidget):
 		self.pointList[-1] = QtCore.QPointF(xNew, yNew)
  		self.pointList.append(QtCore.QPointF(xNew, 0.0))
 
-		self.pointList = [QtCore.QPointF(self.xData[0], 0)]
-		self.pointList = [ QtCore.QPointF(x, self.yData[ind]) for ind, x in enumerate(self.xData[0:self.xPos])]
-		self.pointList.append(QtCore.QPointF(self.xData[self.xPos], 0.0))
+		self.pointList = [QtCore.QPointF(self.xData[0], -1)]
+		self.pointList = [ QtCore.QPointF(x, self.yData[ind]) for ind, x in enumerate(self.xData[0:self.xPos+1])]
+		self.pointList.append(QtCore.QPointF(self.xData[self.xPos], -1.0))
  		
  		self.dataPoly = QtGui.QPolygonF(self.pointList)
 		
@@ -100,7 +104,7 @@ class SmallTrend(QtGui.QWidget):
 
 		transform = QtGui.QTransform()
 #		transform = QtGui.QTransform.fromTranslate(self.width()-self.axisOffset, self.height()-self.axisOffset)
-		transform.translate(self.width()-self.axisOffset, self.height()-self.axisOffset+1)
+		transform.translate(self.width()-self.axisOffset-1, self.height()-self.axisOffset-1)
 		transform.scale(self.xScale, -self.yScale)
 		transform.translate(-self.xData[self.xPos], 0)
 		
@@ -112,6 +116,9 @@ class SmallTrend(QtGui.QWidget):
  		bottom = self.height()-self.axisOffset
  		painter = QtGui.QPainter()
  		painter.begin(self)
+ 		painter.setClipRect(self.axisOffset,self.axisOffset-1,self.xSpan*self.xScale,self.ySpan*self.yScale-self.axisOffset/self.yScale+3)
+ 		painter.setClipRect(self.axisOffset,self.axisOffset,self.xSpan*self.xScale,self.ySpan*self.yScale+self.axisOffset/self.yScale)
+ 		painter.setClipRect(self.axisOffset,self.axisOffset,self.xSpan*self.xScale,self.ySpan*self.yScale+self.axisOffset)
   		painter.setRenderHint(QtGui.QPainter.Antialiasing, True)
   		painter.setTransform(transform)  	
   		painter.setPen(self.curvePen)
@@ -120,12 +127,20 @@ class SmallTrend(QtGui.QWidget):
   		painter.setRenderHint(QtGui.QPainter.TextAntialiasing, True)
   		painter.setRenderHint(QtGui.QPainter.SmoothPixmapTransform, True)
   		painter.setWorldMatrixEnabled(False)
+  		painter.setClipping(False)
+  		painter.setPen(self.axisBkgPen)
+  		painter.setBrush(self.axisBkgBrush)
+  		sx = str(self.xSpan)
+  		sy = str(self.ySpan)
+  		painter.drawRect(right-QtGui.QFontMetricsF(self.axisFont).width(sy)-1, self.axisOffset, 
+						QtGui.QFontMetricsF(self.axisFont).width(sy)+1, self.axisFontHeight-3)
+  		painter.drawRect(self.axisOffset+2, bottom-self.axisFontHeight+2, 
+						QtGui.QFontMetricsF(self.axisFont).width(sx)+1, self.axisFontHeight-3)
   		painter.setPen(self.axisPen)
   		painter.setBrush(self.axisBrush)
   		painter.setFont(self.axisFont)
-  		painter.drawText(self.axisOffset+2, bottom-1, str(self.xSpan))
-  		s = str(self.ySpan)
-  		painter.drawText(right-QtGui.QFontMetricsF(self.axisFont).width(s)-2, self.axisOffset+self.axisFontHeight-4, s)
+  		painter.drawText(self.axisOffset+2, bottom-1, sx)
+  		painter.drawText(right-QtGui.QFontMetricsF(self.axisFont).width(sy)-2, self.axisOffset+self.axisFontHeight-4, sy)
   		painter.setRenderHint(QtGui.QPainter.Antialiasing, False)
   		painter.drawRect(self.axisOffset, self.axisOffset, right - self.axisOffset, bottom - self.axisOffset)
  		painter.end()
@@ -146,7 +161,7 @@ class SmallTrend(QtGui.QWidget):
 		if self.xNum < 2:
 			self.pointList = [QtCore.QPointF(self.width()-self.axisOffset-0*self.xScale, self.height()-self.axisOffset-0*self.yScale), 
 						QtCore.QPointF(self.width()-self.axisOffset-0*self.xScale, self.height()-self.axisOffset-0*self.yScale)]
-			self.pointList = [QtCore.QPointF(0.0, 0.0),	QtCore.QPointF(0.0, 0.0)]
+			self.pointList = [QtCore.QPointF(0.0, -1.0),	QtCore.QPointF(0.0, -1.0)]
 		else:
 	 		hOff = self.width()-self.axisOffset
 	 		vOff = self.height()-self.axisOffset
@@ -155,9 +170,9 @@ class SmallTrend(QtGui.QWidget):
 			self.pointList = [ QtCore.QPointF(hOff-(x - self.xData[0])*self.xScale, vOff-self.yData[ind]*self.yScale) for ind, x in enumerate(self.xData[0:self.xPos])]
 			self.pointList.append(QtCore.QPointF(self.width()-self.axisOffset-(self.xData[self.xPos] - self.xData[0])*self.xScale, self.height()-self.axisOffset))
 		
-			self.pointList = [QtCore.QPointF(0, 0)]
+			self.pointList = [QtCore.QPointF(0, -1)]
 			self.pointList = [ QtCore.QPointF(x, self.yData[ind]) for ind, x in enumerate(self.xData[0:self.xPos])]
-			self.pointList.append(QtCore.QPointF(self.xData[self.xPos], 0.0))
+			self.pointList.append(QtCore.QPointF(self.xData[self.xPos], -1.0))
 
 		
 class TrendTest(QtGui.QWidget):
@@ -166,18 +181,24 @@ class TrendTest(QtGui.QWidget):
 
 		self.deviceName = 'testfel/gunlaser/osc_spectrometer'
 	
-		self.device = pt.DeviceProxy(self.deviceName)
+#		self.device = pt.DeviceProxy(self.deviceName)
 		
 		layout = QtGui.QVBoxLayout(self)
 		layout2 = QtGui.QHBoxLayout(self)
+		layout3 = QtGui.QHBoxLayout(self)
 		self.trendWidget = SmallTrend()
+		self.trendWidget2 = SmallTrend()
 		label = QtGui.QLabel(''.join(('Trending ', self.deviceName)))
 		self.labelData = QtGui.QLabel('')
+		self.labelData2 = QtGui.QLabel('')
 
 		layout.addWidget(label)
 		layout2.addWidget(self.trendWidget)
 		layout2.addWidget(self.labelData)
+		layout3.addWidget(self.trendWidget2)
+		layout3.addWidget(self.labelData2)
 		layout.addLayout(layout2)
+		layout.addLayout(layout3)
 
 		s = '''QWidget { background-color : #000000;
 			color : #66ccff; }
@@ -188,13 +209,17 @@ class TrendTest(QtGui.QWidget):
 		
 		self.timer = QtCore.QTimer()
 		QtCore.QObject.connect(self.timer, QtCore.SIGNAL("timeout()"), self.updateData)
-		self.timer.start(500)
+		self.timer.start(200)
 		
 	def updateData(self):
-		attr = self.device.read_attribute('PeakEnergy')
-		t = attr.value
-		self.labelData.setText(str(int(t)))
+		t = np.random.rand()
+		self.labelData.setText('{:03.2f}'.format(t))
 		self.trendWidget.updateData(time.time(), t)
+
+		t = (np.sin(time.time()*0.2)+1+0.2*np.random.rand()-0.1)/2
+#		t = (np.sin(time.time()*0.75)+1)/2
+		self.labelData2.setText('{:03.2f}'.format(t))
+		self.trendWidget2.updateData(time.time(), t)
 		
 
 if __name__ == '__main__':
